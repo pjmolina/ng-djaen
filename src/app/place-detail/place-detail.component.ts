d5ad7ccd8153024f7a7f7fa2df974e0fb371ff0a
Pedro J. Molina <pjmolina@gmail.com>
2021-11-03 14:20:38 +0100

    PlaceDetail

--- src/app/place-detail/place-detail.component.ts
+++ src/app/place-detail/place-detail.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Place } from '../domain/place';
+import { PlaceService } from '../services/place.service';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.component.html',
-  styleUrls: ['./place-detail.component.scss']
+  styleUrls: ['./place-detail.component.scss'],
 })
-export class PlaceDetailComponent implements OnInit {
+export class PlaceDetailComponent implements OnInit, OnDestroy {
+  place?: Place;
+  cargando = false;
+  sub?: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private placeService: PlaceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    console.log('Componente construido');
   }
 
+  ngOnInit(): void {
+    console.log('Componente init');
+    this.sub = this.route.params.subscribe((data) => {
+      const id = data.id;
+      console.log('Campio de parametros: id', id);
+      if (id) {
+        this.cargando = true;
+        this.placeService.getPlaceById(id).subscribe({
+          next: (place) => {
+            this.cargando = false;
+
+            this.place = place;
+          },
+          error: (err) => {
+            this.cargando = false;
+            this.place = undefined;
+          },
+        });
+      } else {
+        this.place = undefined;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    console.log('Componente destruido');
+    this.sub?.unsubscribe();
+  }
 }
